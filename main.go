cadc573e810b6232bcd03eeb620e59d0b8985430
DESKTOP-RKMKLHA\owner <2825873215@QQ.COM>
2021-12-13 11:25:13 +0800

    add port flag

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/miaogaolin/printlove-go/handler"
 
 	"github.com/gin-gonic/gin"
 )
 
+var post = flag.String("p", "8080", "port")
+
 func main() {
+	flag.Parse()
 	r := gin.New()
 	handler.Route(r)
-	Server(r, "8080")
+	Server(r, *post)
 }
